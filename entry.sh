5c6cebcaf596d6324e84c9f43b6a1dff9cefac79
Steven <steven001@gmail.com>
2023-08-29 09:35:19 +1000

    feat: add additional ssh, git, and ansible environment setup

--- entry.sh
+++ entry.sh
@@ -2,14 +2,19 @@
 
 set -exuo pipefail
 
-## Install Ansible inventory file.
+## Set up Ansible environment
+git config --global --add safe.directory /drone/src
+echo "${ANSIBLE_VAULT_PASSWORD:?not set}" >$HOME/.ansible_vault
+
 mkdir -p /etc/ansible
 echo -e "[local]\nlocalhost ansible_connection=local ansible_user=root" >/etc/ansible/hosts
 
 ## enable local ssh
 ssh-keygen -A
-mkdir -p -m 0600 /root/.ssh
+mkdir -p /root/.ssh
+echo "${DRONE_SSH_KEY}" >/root/.ssh/id_ed25519
 echo "${DRONE_SSH_PUB_KEY}" >/root/.ssh/authorized_keys
+chmod -R 600 /root/.ssh
 echo -e "PasswordAuthentication no" >>/etc/ssh/sshd_config
 
 ## start sshd
